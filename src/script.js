e62aaca5c9cf6b173decaa5b31195c5c02841cb4
juliap <juliapwols@gmail.com>
2025-01-12 18:39:10 -0300

    Projetos, rodapé, imagens.

--- src/script.js
+++ src/script.js
@@ -3,19 +3,22 @@ const idade = (new Date() - aniversario) / (1000 * 60 * 60 * 24 * 365);
 
 document.getElementById("minha-idade").textContent = `${Math.floor(idade)} anos`;
 
-let ultimaImagem = -1;
+
+const imgArr = [
+    "entrapta icon.jpg", "jacko.jpg", "marcille.png", "ramlethal.png", "pretty aqua closeup.png"
+];
+let imgDisp = imgArr.slice();
 
 const imagemAleatoria = () => {
-    const imgArr = [
-        "entrapta icon.jpg", "jacko.jpg", "marcille.png", "ramlethal.png", "pretty aqua closeup.png"
-    ];
+    const x = Math.floor(Math.random() * imgDisp.length);
+    let img = "eu.png";
 
-    const getImagemAleatoria = () => {
-        const x = Math.floor(Math.random() * imgArr.length);
-        ultimaImagem = ultimaImagem === x ? getImagemAleatoria() : x;
-        return ultimaImagem;
+    if (imgDisp.length > 0) {
+        img = imgDisp[x];
+        imgDisp.splice(x, 1);
+    } else {
+        imgDisp = imgArr.slice();
     }
 
-    const img = imgArr[getImagemAleatoria()];
     document.getElementById("eu-img").src = `assets/${img}`;
 }
