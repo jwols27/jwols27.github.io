fd7e1d6de6ddfc50c386fb41a26f8e75a4cd650c
Júlia Patricia Wolschick <juliapwols@gmail.com>
2025-03-03 00:06:24 -0300

    feat: refatora html, js; melhora css

--- src/script.js
+++ src/script.js
@@ -1,3 +1,37 @@
+// Localization
+
+async function carregarLocale(lang) {
+    const res = await fetch("src/lang.json");
+    const localizacao = await res.json();
+
+    document.querySelectorAll("[data-i18n]").forEach((element) => {
+        const key = element.getAttribute("data-i18n");
+        if (localizacao[lang] && localizacao[lang][key]) {
+            element.textContent = localizacao[lang][key];
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const locale = localStorage.getItem("locale") ?? "pt";
+    carregarLocale(locale).then(_ => {
+    });
+});
+
+function mudarLocale() {
+    let locale = localStorage.getItem("locale") ?? "pt";
+    locale = locale === "pt" ? "en" : "pt"
+    localStorage.setItem("locale", locale);
+    carregarLocale(locale).then(_ => {
+    });
+}
+
+// Scripting
+const aniversario = new Date('2003/01/27');
+const idade = (new Date() - aniversario) / (1000 * 60 * 60 * 24 * 365);
+
+document.getElementById("minha-idade").textContent = Math.floor(idade).toLocaleString();
+
 const imgArr = [
     "entrapta icon.jpg", "jacko.jpg", "marcille.png", "ramlethal.png", "pretty aqua closeup.png"
 ];
